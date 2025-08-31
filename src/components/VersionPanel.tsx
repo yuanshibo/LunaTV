66bce568b4c1478375d178036d53bcaf3b4dab3d
ysb <yuanshibo@live.com>
2025-08-31 12:04:10 +0000

    VersionPanel

--- src/components/VersionPanel.tsx
+++ src/components/VersionPanel.tsx
@@ -81,7 +81,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
   const fetchRemoteChangelog = async () => {
     try {
       const response = await fetch(
-        'https://raw.githubusercontent.com/MoonTechLab/LunaTV/main/CHANGELOG'
+        'http://192.168.10.103:3002/yuan/LunaTV/src/branch/main/CHANGELOG'
       );
       if (response.ok) {
         const content = await response.text();
@@ -363,7 +363,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
                     </div>
                   </div>
                   <a
-                    href='https://github.com/MoonTechLab/LunaTV'
+                    href='http://192.168.10.103:3002/yuan/LunaTV'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-yellow-600 hover:bg-yellow-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
@@ -393,7 +393,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
                     </div>
                   </div>
                   <a
-                    href='https://github.com/MoonTechLab/LunaTV'
+                    href='http://192.168.10.103:3002/yuan/LunaTV'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-green-600 hover:bg-green-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
