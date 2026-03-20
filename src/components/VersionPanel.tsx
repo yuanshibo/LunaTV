10c13c2598b58e3381208f768fb5a2f0de76ec29
yuanshibo <yuanshibo@live.com>
2026-03-21 02:01:43 +0900

    Update remote changelog URL in VersionPanel

--- src/components/VersionPanel.tsx
+++ src/components/VersionPanel.tsx
@@ -81,7 +81,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
   const fetchRemoteChangelog = async () => {
     try {
       const response = await fetch(
-        'https://raw.githubusercontent.com/MoonTechLab/LunaTV/main/CHANGELOG'
+        'https://raw.githubusercontent.com/yuanshibo/LunaTV/main/CHANGELOG'
       );
       if (response.ok) {
         const content = await response.text();
